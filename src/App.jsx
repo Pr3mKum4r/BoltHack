0218f96aea17e8a78a60355e33a59f2499a3fe27
Krishnan9074 <vkrishnan9074@gmail.com>
2024-02-23 03:28:31 +0530

    youtube search

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,11 @@ import Signup from './components/Signup'
 import Landing from './components/Landing';
 import Footer from './components/Footer';
 import GeminiPro from './components/GeminiPro';
+<<<<<<< Updated upstream
 import Autism from './components/Autism';
+=======
+import SearchBar from './components/YoutubeSearch';
+>>>>>>> Stashed changes
 function App() {
 
   return (
@@ -19,7 +23,11 @@ function App() {
             <Route path="/login" exact element={<Login />} />
             <Route path="/signup" exact element={<Signup />} />
             <Route path="/gemini" exact element={<GeminiPro />} />
+<<<<<<< Updated upstream
             <Route path="/autism" exact element={<Autism/>} />
+=======
+            <Route path="/yt" exact element={<SearchBar/>} />
+>>>>>>> Stashed changes
           </Routes>
           <Footer/>
         </UserAuthContextProvider>
